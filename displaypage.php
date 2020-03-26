614aab74e76b45be64e9cfdc517bda22a32c5760
cmayouma <christianmay@hotmail.fr>
2020-03-26 16:54:20 +0100

    tp pas terminé, je n'ai pas compris beaucoup d'instructions.

---
+++ displaypage.php
@@ -0,0 +1,29 @@
+<?php
+<! doctype html>
+<html dir="ltr" lang="fr">
+<head>
+<div class="scrollmenu">
+  <a href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+ </div> 
+</head>
+<body>
+div.scrollmenu {
+      background-color: #333;
+     overflow: auto;
+     white-space: nowrap;
+    }...
+</body>
+div.scrollmenu a {
+      display: inline-block;
+        margin: 8px;
+        color: white;
+        text-align: center;
+        padding: 14px;
+         text-decoration: none;
+    }
+    div.scrollmenu a:hover {
+          background-color: #777;
+?>
---
+++ homepage.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Document</title>
+
+
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+    
+
+    <div class="container">
+    getTvShow(id,)
+    var_dump(id)
+
+        <form method="post" action="login.php">
+
+            <div class="form-group">
+
+                <label for="exampleInputSeriename1">Seriename</label>
+
+                <input name="seriename" type="seriename" class="form-control" id="exampleInputSeriename1" aria-describedby="serienamelHelp">
+
+
+                <label for="exampleInputSeasonslist">Seasonslist</label>
+                <input name="seasonslist" type="seasonslist" class="form-control" id="exampleInputseasonslist1" aria-describedby="seasonslistlHelp">
+
+
+</body>
+
+</html>
